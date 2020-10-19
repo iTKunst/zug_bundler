641b8c8d07e1f364c81c0ca029c19562327b2e9e
TAGHRdooleyp <pjldooley@gmail.com>
2020-10-18 22:49:05 -0400

    aa

--- git/win/pADD_REPO.cmd
+++ git/win/pADD_REPO.cmd
@@ -9,7 +9,7 @@ CALL LOG_VAR GIT %GIT%
 CALL LOG_VAR DIR %DIR%
 
 git clone %GIT% %DIR%
-:: git commit -m "adding submodule %DIR%"
-:: git push
+git commit -m "adding submodule %DIR%"
+git push
 
 CALL LOG_EXIT pCLONE_REPO
