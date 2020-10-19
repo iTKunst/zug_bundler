cc277e38f58b54fc0f0b4bf04bb2f8c88ec5780a
TAGHRdooleyp <pjldooley@gmail.com>
2020-10-18 22:44:56 -0400

    aa

--- git/win/pCLONE_REPO.cmd
+++ git/win/pCLONE_REPO.cmd
@@ -8,11 +8,11 @@ SET DIR=%2
 CALL LOG_VAR GIT %GIT%
 CALL LOG_VAR DIR %DIR%
 
-REM git clone %GIT% %DIR%
+git clone %GIT% %DIR%
 
-echo "git clone %GIT% %DIR%"
-SET SRC=..\..\tmpl\tmpl_%DIR%
-CALL LOG_VAR SRC %SRC%
-xcopy /q /y %SRC% .\%DIR%
+REM echo "git clone %GIT% %DIR%"
+REM SET SRC=..\..\tmpl\tmpl_%DIR%
+REM CALL LOG_VAR SRC %SRC%
+REM xcopy /q /y %SRC% .\%DIR%
 
 CALL LOG_EXIT pCLONE_REPO
