082df3f7e94655060e294a13c58e782ead95dfa0
TAGHRdooleyp <pjldooley@gmail.com>
2020-10-21 02:16:17 -0400

    aa

--- log/win/LOG_VAR.cmd
+++ log/win/LOG_VAR.cmd
@@ -22,7 +22,7 @@ echo PARAMS is %PARAMS%
 SET "VAR=%1"
 echo VAR is %VAR%
 
-SET "VAL=%2"
+SET "VAL=%PARAMS:("%VAR% ")=%"
 echo VAL is "%VAL%"
 
 CALL .\Bundler\log\win\LOG_MSG "%VAR% is ("%VAL%") [VAR]"
