865e420fcf5c24e17587a422567a039fc444bf9f
TAGHRdooleyp <pjldooley@gmail.com>
2020-10-21 03:03:51 -0400

    aa

--- log/win/LOG_VAR.cmd
+++ log/win/LOG_VAR.cmd
@@ -29,7 +29,7 @@ echo VAR is %length% characters long
 SET "VAL=%PARAMS:* =%"
 echo VAL is "%VAL%"
 
-CALL .\Bundler\log\win\LOG_MSG "%VAR% is ("%VAL%") [VAR]"
+CALL .\Bundler\log\win\LOG_MSG "%VAR% is (%VAL%) [VAR]"
 
 :EOF
 
