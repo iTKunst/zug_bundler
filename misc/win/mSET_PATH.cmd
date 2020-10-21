63bb96be7e3ffdea173c4dac5a668a4733ae9647
TAGHRdooleyp <pjldooley@gmail.com>
2020-10-20 23:41:50 -0400

    aa

--- misc/win/mSET_PATH.cmd
+++ misc/win/mSET_PATH.cmd
@@ -4,16 +4,13 @@ CALL .\Bundler\log\win\LOG_ENTER mSET_PATH
 
 CALL .\Bundler\log\win\LOG_VAR PATH %PATH%
 
-SET FIND=%CD%\Exe
+SET FIND=%CD%\Exe;%CD%\Env
 CALL .\Bundler\log\win\LOG_VAR FIND %FIND%
 
-echo %PATH% | findstr "%CD%\Exe" 1>nul
-CALL .\Bundler\log\win\LOG_CMD "findstr called"
-
-if errorlevel 1 (
+echo %PATH% | find/ I "%FIND%">Nul && (
   CALL .\Bundler\log\win\LOG_DUP PATH
 ) else (
-  set PATH=%CD%\Exe;%CD%\Env;%PATH%
+  SET PATH=%FIND%;%PATH%
 )
 
 CALL .\Bundler\log\win\LOG_VAR PATH %PATH%
