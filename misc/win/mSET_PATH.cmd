7296609b7af34274f964450bf1f730ec248a6f6d
TAGHRdooleyp <pjldooley@gmail.com>
2020-10-21 03:17:26 -0400

    aa

--- misc/win/mSET_PATH.cmd
+++ misc/win/mSET_PATH.cmd
@@ -10,7 +10,7 @@ Echo.Hello world | findstr /C:"world">nul && (
 
 CALL .\Bundler\log\win\LOG_VAR PATH %PATH%
 
-SET FIND=%CD%\Exe
+SET "FIND=%CD%\Exe"
 CALL .\Bundler\log\win\LOG_VAR FIND %FIND%
 
 echo "%PATH%" | findstr /C:"%FIND%">nul && (
