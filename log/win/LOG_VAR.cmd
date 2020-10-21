154832febc3b0c1cae935fd955e6bc3f5cb70de1
TAGHRdooleyp <pjldooley@gmail.com>
2020-10-21 02:55:51 -0400

    aa

--- log/win/LOG_VAR.cmd
+++ log/win/LOG_VAR.cmd
@@ -1,4 +1,5 @@
 @echo off
+setlocal enabledelayedexpansion
 CALL .\settings
 
 :: echo %bSETTING_LOG_VAR%
@@ -22,9 +23,23 @@ echo PARAMS is %PARAMS%
 SET "VAR=%1"
 echo VAR is %VAR%
 
+CALL :COUNT "%VAR%"
+echo VAR is %length% characters long
+
 SET "VAL=%PARAMS:("%VAR%")=%"
 echo VAL is "%VAL%"
 
 CALL .\Bundler\log\win\LOG_MSG "%VAR% is ("%VAL%") [VAR]"
 
 :EOF
+
+
+:COUNT
+set temp=%1
+set length=0
+:loop
+if defined temp (
+    set temp=!temp:~1!
+    set /a length+=1
+    goto :loop
+)
