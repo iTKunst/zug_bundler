ab16098aaaaf455e7c6f881d654b65018caed6dc
TAGHRdooleyp <pjldooley@gmail.com>
2020-10-21 03:02:39 -0400

    aa

--- log/win/LOG_VAR.cmd
+++ log/win/LOG_VAR.cmd
@@ -26,7 +26,7 @@ echo VAR is %VAR%
 CALL :COUNT "%VAR%"
 echo VAR is %length% characters long
 
-SET "VAL=%PARAMS:("%VAR%")=%"
+SET "VAL=%PARAMS:* =%"
 echo VAL is "%VAL%"
 
 CALL .\Bundler\log\win\LOG_MSG "%VAR% is ("%VAL%") [VAR]"
