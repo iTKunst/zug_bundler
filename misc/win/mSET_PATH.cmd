dcdc2172a8cdb9825045a1e2075d4618b56b57dd
TAGHRdooleyp <pjldooley@gmail.com>
2020-10-21 03:39:13 -0400

    aa

--- misc/win/mSET_PATH.cmd
+++ misc/win/mSET_PATH.cmd
@@ -14,13 +14,13 @@ echo "%PATH%" | findstr /C:"%FIND%">nul && (
   SET "FOUND=0"
 )
 
+CALL .\Bundler\log\win\LOG_VAR FOUND %FOUND%
+
 IF %FOUND% EQU 0 (
   SET PATH=%CD%\Exe;%CD\Env;%PATH%
-  SET PATH 
 REM  SET "PATH_CMD=SET PATH^=%PATH_NEW%"
 REM   echo %PATH_CMD% >path.cmd
 )
 
-CALL .\Bundler\log\win\LOG_VAR FOUND %FOUND%
 
 CALL .\Bundler\log\win\LOG_EXIT mSET_PATH
