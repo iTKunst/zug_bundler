e44bf21fa172bf930dcef01aa66f054d21b4abe4
TAGHRdooleyp <pjldooley@gmail.com>
2020-10-22 08:39:12 -0400

    aa

--- git/linux/pSET_TRACE.sh
+++ git/linux/pSET_TRACE.sh
@@ -14,12 +14,14 @@ log_var bSETTING_LOG_GIT $bSETTING_LOG_GIT
 
 if [ "$bSETTING_LOG_GIT" == "true" ]; then
   log_info "Turning Git tracing on"
-  export GIT_TRACE=1
   export GIT_CURL_VERBOSE=1
+  export GIT_TRACE=1
+  export GIT_TRACE_PACKET=1
 else
   log_info "Turning Git tracing off"
-  unset GIT_TRACE
   unset GIT_CURL_VERBOSE
+  unset GIT_TRACE
+  unset GIT_TRACE_PACKET
 fi
 
 log_exit pSET_TRACE
