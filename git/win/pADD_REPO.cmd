2e72986428492cf8d5592073025cf8856fe2b806
TAGHRdooleyp <pjldooley@gmail.com>
2020-10-22 08:59:03 -0400

    aa

--- git/win/pADD_REPO.cmd
+++ git/win/pADD_REPO.cmd
@@ -1,20 +1,16 @@
 @echo off	
 
-CALL LOG_ENTER pCLONE_REPO
+CALL LOG_ENTER pADD_REPO
 
 SET GIT=%1
 SET DIR=%2
 
-CALL pGET_URI
-
-SET "URI=%PROJ_URI%%SLASH%%GIT%"
-
-CALL LOG_VAR URI %URI%
+CALL LOG_VAR GIT %GIT%
 CALL LOG_VAR DIR %DIR%
 
-git clone %URI% %DIR%
+git clone %GIT% %DIR%
 git commit -m "adding submodule %DIR%"
 git push
 git submodule update --init --recursive
 
-CALL LOG_EXIT pCLONE_REPO
+CALL LOG_EXIT pADD_REPO
