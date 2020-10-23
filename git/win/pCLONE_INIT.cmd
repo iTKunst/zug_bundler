21a933b5ea5cdfcc2a2924d537e8631d9eff9313
TAGHRdooleyp <pjldooley@gmail.com>
2020-10-23 01:01:37 -0400

    jj

--- git/win/pCLONE_INIT.cmd
+++
@@ -1,8 +0,0 @@
-@echo off
-
-CALL LOG_ENTER pCLONE_INIT
-
-git submodule init
-git submodule update
-
-CALL LOG_EXIT pCLONE_INIT
--- misc/win/mSET_PATH.cmd
+++ misc/win/mSET_PATH.cmd
@@ -2,22 +2,18 @@ REM @echo off
 
 CALL .\Bundler\log\win\LOG_ENTER mSET_PATH
 
-REM CALL .\Bundler\log\win\LOG_VAR PATH %PATH%
+CALL .\Bundler\log\win\LOG_VAR PATH %PATH%
 
 SET "FIND=%CD%\Exe"
-REM CALL .\Bundler\log\win\LOG_VAR FIND %FIND%
+CALL .\Bundler\log\win\LOG_VAR FIND %FIND%
 
 echo "%PATH%" | findstr /C:"%FIND%">nul && (
-  ECHO.FALSE
   GOTO :EOF
 )
 
 SET "PATH=%CD%\Exe;%CD%\Env;%PATH%"
-REM  SET "PATH_CMD=SET PATH^=%PATH_NEW%"
 echo "SET PATH=%CD%\Exe;%CD%\Env;%PATH%" >path.cmd
-type path.cmd
 
-REM CALL .\Bundler\log\win\LOG_VAR PATH %PATH%
 
 :EOF
 
