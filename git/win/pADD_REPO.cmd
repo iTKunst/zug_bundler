f33f614a2340f5e271ae07ef68f84b5915476135
TAGHRdooleyp <pjldooley@gmail.com>
2020-10-23 00:14:22 -0400

    jj

--- git/win/pADD_REPO.cmd
+++ git/win/pADD_REPO.cmd
@@ -8,9 +8,8 @@ SET DIR=%2
 CALL LOG_VAR GIT %GIT%
 CALL LOG_VAR DIR %DIR%
 
-git clone %GIT% %DIR%
+git submodule add -f %GIT% %DIR%
 git commit -m "adding submodule %DIR%"
 git push
-git submodule update --init --recursive
 
 CALL LOG_EXIT pADD_REPO
