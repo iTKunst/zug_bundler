a99314da417885f8d3a06d6ec7b259bb0c2a773c
TAGHRdooleyp <pjldooley@gmail.com>
2020-10-23 15:16:14 -0400

    jj

--- env/linux/bENV_GIT.sh
+++ env/linux/bENV_GIT.sh
@@ -7,34 +7,33 @@ log_enter bENV_GIT
 source bENV_CAT.sh
 source bENV_TKN.sh
 
-if [ -z "$TMPL_URI" ]; then
-  log_invalid TMPL_URI
-  log_cmd "You must set it in ./env.sh!"
-  log_cmd "Default value is https://github.com/itkunst"
-  exit
-fi
-log_var TMPL_URI $TMPL_URI
-
-if [ -z "$TMPL_NAME" ]; then
-  log_invalid TMPL_NAME
-  log_cmd "You must set it in ./env.sh!"
-  exit
-fi
-echo TMPL_NAME "$TMPL_NAME"
-
 if [ -z "$SYS" ]; then
   log_invalid SYS
   log_cmd "You must set it in ./env.sh!"
   exit
 fi
-echo SYS "$SYS"
+log_var SYS $SYS
 
-if [ -z "$SYS_GIT" ]; then
+if [ -z $SYS_GIT ]; then
   log_invalid SYS_GIT
   log_cmd "You must set it in ./env.sh!"
   exit
 fi
-echo SYS_GIT "$SYS_GIT"
+echo SYS_GIT $SYS_GIT
+
+if [ -z $TMPL_NAME ]; then
+  log_invalid TMPL_NAME
+  log_cmd "You must set it in ./env.sh!"
+  exit
+fi
+echo TMPL_NAME $TMPL_NAME
+
+if [ -z $TMPL_URI ]; then
+  log_invalid TMPL_URI
+  log_cmd "You must set it in ./env.sh!"
+  exit
+fi
+echo TMPL_URI $TMPL_URI
 
 export GLBL_GIT=$TMPL_URI$SLASH"zug_global.git"
 log_var GLBL_GIT $GLBL_GIT
