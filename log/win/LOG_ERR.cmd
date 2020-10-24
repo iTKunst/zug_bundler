c0a5ee13a76e082353de27c17f1be3c61ac287a7
TAGHRdooleyp <pjldooley@gmail.com>
2020-10-24 08:44:12 -0400

    AA

---
+++ log/win/LOG_ERR.cmd
@@ -0,0 +1,23 @@
+@echo off
+CALL .\settings
+
+:: echo %bSETTING_LOG_ENTER%
+
+
+if NOT DEFINED bSETTING_LOG_ERR (
+::  echo not defined
+  GOTO :EOF
+)
+::echo is defined
+
+IF /I "%bSETTING_LOG_ERR%"=="FALSE" (
+::  echo is false
+  GOTO :EOF
+)
+:: echo is true
+
+CALL .\Bundler\log\win\LOG_MSG "(%*) [ERROR]"
+
+:EOF
+
+
