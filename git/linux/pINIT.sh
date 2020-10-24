d549d225743fbf7970a75a4975af51027e151701
TAGHRdooleyp <pjldooley@gmail.com>
2020-10-24 13:47:01 -0400

    aa

---
+++ git/linux/pINIT.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+# shellcheck disable=SC2086
+source ./Bundler/log/linux/LOG.sh
+
+log_enter init
+
+if [ ! -d Exe ]; then
+  mkdir Exe
+fi
+
+if [ ! -d Env ]; then
+  mkdir Exe
+fi
+mkdir Env
+
+source ./$BNDL_DIR/init.sh
+source ./Exe/mSET_PATH.sh
+
+CLONE=0
+
+if [ -d Global ]; then
+  source ./Global/init.sh
+else
+  CLONE=1
+fi
+
+if [ -d System ]; then
+   source ./System/init.sh
+else
+  CLONE=1
+fi
+
+if [ -d Project ]; then
+   source ./Project/init.sh
+else
+  CLONE=1
+fi
+
+chmod +x ./Exe/*.sh
+
+if [ $CLONE -eq 1  ]; then
+  source pCLONE.sh
+fi
+
+log_exit init
