2ee62813269a2d595d74bfbbe6552a3145888a59
TAGHRdooleyp <pjldooley@gmail.com>
2020-10-24 21:16:34 -0400

    aa

--- git/linux/pINIT.sh
+++ git/linux/pINIT.sh
@@ -16,21 +16,26 @@ source ./$BNDL_DIR/init.sh
 source ./Exe/mSET_PATH.sh
 
 source pINIT_GLBL.sh
+log_var ERRORLEVEL $ERRORLEVEL
 if [ $ERRORLEVEL -ne 0 ]; then
 	exit $ERRORLEVEL
 fi
 
 source pINIT_PROJ.sh
+log_var ERRORLEVEL $ERRORLEVEL
 if [ $ERRORLEVEL -ne 0 ]; then
 	exit $ERRORLEVEL
 fi
 
 source pINIT_SYS.sh
+log_var ERRORLEVEL $ERRORLEVEL
 if [ $ERRORLEVEL -ne 0 ]; then
 	exit $ERRORLEVEL
 fi
 
 chmod +x ./Exe/*.sh
+
+log_var CLONE $CLONE
 
 if [ $CLONE -eq 1  ]; then
   source pCLONE.sh
