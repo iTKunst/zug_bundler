7c9a37eb3b6d90d9b2cf4e0bd475b17ef6d783c7
TAGHRdooleyp <pjldooley@gmail.com>
2020-10-26 14:58:55 -0400

    aa

--- init.cmd
+++ init.cmd
@@ -1,11 +1,11 @@
 @echo off
+CALL .\$DIR_BNDL\log\win\LOG
 
-
-CALL .\$DIR_BNDL\log\win\LOG_ENTER \$DIR_BNDL\init
+CALL :log_enter \$DIR_BNDL\init
 
 CALL .\$DIR_BNDL\env\init
 CALL .\$DIR_BNDL\git\init
 CALL .\$DIR_BNDL\log\init
 CALL .\$DIR_BNDL\misc\init
 
-CALL .\$DIR_BNDL\log\win\LOG_EXIT \$DIR_BNDL\init
+CALL :log_exit \$DIR_BNDL\init
