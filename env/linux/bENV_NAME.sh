a457532fc8e1fcdfcc2ccaebb1212246e94b6918
TAGHRdooleyp <pjldooley@gmail.com>
2020-10-27 09:26:23 -0400

    aa

--- env/linux/bENV_NAME.sh
+++ env/linux/bENV_NAME.sh
@@ -5,7 +5,6 @@
 log_enter bENV_NAME
 
 export GLBL_NAME=base_global
-export BASE_NAME=base_tmpl
 
 log_exit bENV_NAME
 
--- env/win/bENV_NAME.cmd
+++ env/win/bENV_NAME.cmd
@@ -4,6 +4,5 @@
 call LOG_ENTER bENV_NAME
 
 SET "GLBL_NAME=base_global"
-SET "BASE_NAME=base_tmpl"
 
 call LOG_EXIT bENV_CAT
