a4c47953e8bf9205ba7a940952ebbd8fcd376579
TAGHRdooleyp <pjldooley@gmail.com>
2020-10-27 22:33:34 -0400

    aa

--- git/win/pINIT.cmd
+++ git/win/pINIT.cmd
@@ -25,9 +25,6 @@ CALL LOG_VAR DIR_SYS %DIR_SYS%
 
 if exist %DIR_SYS% (
   CALL .\%DIR_SYS%\init
-	if %ERRORLEVEL% NEQ 0 (
-	  echo system\init caused error
-    SET "CLONE=1"
 ) else (
   SET "CLONE=1"
 )
