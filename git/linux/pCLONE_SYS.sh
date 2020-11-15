3db3a5cc23692820951431d750ca8379c69078a9
TAGHRdooleyp <pjldooley@gmail.com>
2020-11-15 17:46:40 -0500

    a

--- git/linux/pCLONE_SYS.sh
+++ git/linux/pCLONE_SYS.sh
@@ -13,19 +13,19 @@ pCLONE_SYS () {
 	fi
 	log_var DIR_SYS $DIR_SYS
 
-	if [ -z $URI_SYS_GIT ]; then
-		log_invalid URI_SYS_GIT
+	if [ -z $SYS_GIT ]; then
+		log_invalid SYS_GIT
 		exit
 	fi
-	log_var URI_SYS_GIT $URI_SYS_GIT
+	log_var SYS_GIT $SYS_GIT
 
 	export init=0
 
 	if [ ! -d $DIR_SYS ]; then
-		pADD_SUB $URI_SYS_GIT $DIR_SYS
+		pADD_SUB $SYS_GIT $DIR_SYS
 		export init=1
 	else
-		log_dup $URI_SYS_GIT
+		log_dup $SYS_GIT
 	fi
 
 
