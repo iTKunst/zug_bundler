8f9871bf2610448d3d72e310cdc3a14fe03312c0
TAGHRdooleyp <pjldooley@gmail.com>
2020-12-04 11:41:33 -0500

    aa

--- misc/linux/mGET_SYS_GIT_BASE_URI.sh
+++ misc/linux/mGET_SYS_GIT_BASE_URI.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 # shellcheck disable=SC2086
 
-sGET_SYS_BASE_URI() {
+sGET_SYS_GIT_BASE_URI() {
 	sss=$(git config --list | grep remote.origin.url)
 	#echo sss is $sss
 	ttt=${sss%=*}
 	#echo ttt is $ttt
 	rrr=$(sed s/remote.origin.url=//g <<< $sss)
 	#echo rrr is $rrr
-	BASE=${rrr%/*}
-	#echo BASE is $BASE
+
+  URI_SYS_GIT_BASE=${rrr%/*}
+	echo URI_SYS_GIT_BASE is $URI_SYS_GIT_BASE
 }
 
-sGET_SYS_BASE_URI
+sGET_SYS_GIT_BASE_URI
