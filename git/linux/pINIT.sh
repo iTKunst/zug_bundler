8a0b89e38347dae7458b7483b90b0ce03076436a
TAGHRdooleyp <pjldooley@gmail.com>
2021-02-18 14:52:01 -0500

    ss

--- git/linux/pINIT.sh
+++ git/linux/pINIT.sh
@@ -7,8 +7,8 @@
 source ./$DIR_BNDL/git/linux/pCLONE.sh
 source ./$DIR_BNDL/git/linux/pINIT_BNDL.sh
 source ./$DIR_BNDL/git/linux/pINIT_GLBL.sh
-source ./$DIR_BNDL/git/linux/pINIT_TMPL.sh
 source ./$DIR_BNDL/git/linux/pINIT_SYS.sh
+source ./$DIR_BNDL/git/linux/pINIT_TMPL.sh
 source ./$DIR_BNDL/git/linux/pINIT_PROJ.sh
 source ./$DIR_BNDL/misc/linux/mSET_PATH.sh
 
